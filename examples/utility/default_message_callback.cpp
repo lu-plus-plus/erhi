0201df33f80622a466b98a4e91aadc9dfa4646fd
jl.lu <jllucs@zju.edu.cn>
2022-08-03 10:01:39 +0800

    [polish] dependencies
    - organize modules inside erhi in a better way
    - remove spdlog

--- examples/utility/default_message_callback.cpp
+++ examples/utility/default_message_callback.cpp
@@ -1,6 +1,25 @@
-#include "spdlog/spdlog.h"
+#include <format>
+#include <iostream>
 
 #include "default_message_callback.hpp"
+
+
+
+#define ERHI_LOG_FORMAT_BEGIN "\033["
+#define ERHI_LOG_FORMAT_CONCAT ";"
+#define ERHI_LOG_FORMAT_END "m"
+
+#define ERHI_LOG_FORE_RED ERHI_LOG_FORMAT_BEGIN "31" ERHI_LOG_FORMAT_END
+#define ERHI_LOG_FORE_GREEN ERHI_LOG_FORMAT_BEGIN "32" ERHI_LOG_FORMAT_END
+#define ERHI_LOG_FORE_YELLOW ERHI_LOG_FORMAT_BEGIN "33" ERHI_LOG_FORMAT_END
+
+#define ERHI_LOG_RESET ERHI_LOG_FORMAT_BEGIN "0" ERHI_LOG_FORMAT_END
+
+#define ERHI_LOG_PREFIX_ERROR ERHI_LOG_FORE_RED "[error]" ERHI_LOG_RESET
+#define ERHI_LOG_PREFIX_WARNING ERHI_LOG_FORE_YELLOW "[warning]" ERHI_LOG_RESET
+#define ERHI_LOG_PREFIX_INFO ERHI_LOG_FORE_GREEN "[info]" ERHI_LOG_RESET
+#define ERHI_LOG_PREFIX_VERBOSE ERHI_LOG_FORE_GREEN "[verbose]" ERHI_LOG_RESET
+#define ERHI_LOG_PREFIX_UNKNOWN ERHI_LOG_FORE_GREEN "[unknown]" ERHI_LOG_RESET
 
 
 
@@ -15,26 +34,23 @@ namespace erhi {
 
 		switch (severity) {
 			case MessageSeverity::Error: {
-				spdlog::error("{}", pMessage);
+				std::cout << std::format("{} {}", ERHI_LOG_PREFIX_ERROR, pMessage) << std::endl;
 			} break;
 
 			case MessageSeverity::Warning: {
-				spdlog::warn("{}", pMessage);
+				std::cout << std::format("{} {}", ERHI_LOG_PREFIX_WARNING, pMessage) << std::endl;
 			} break;
 
 			case MessageSeverity::Info: {
-				spdlog::info("{}", pMessage);
+				std::cout << std::format("{} {}", ERHI_LOG_PREFIX_INFO, pMessage) << std::endl;
 			} break;
 
 			case MessageSeverity::Verbose: {
-				spdlog::set_pattern("[%Y-%m-%d %T.%e] [%^verbose%$] %v");
-				spdlog::info("{}", pMessage);
-				spdlog::set_pattern("%+");
+				std::cout << std::format("{} {}", ERHI_LOG_PREFIX_VERBOSE, pMessage) << std::endl;
 			} break;
 
 			default: {
-				spdlog::warn("message callback: unknown message severity");
-				spdlog::warn("{}", pMessage);
+				std::cout << std::format("{} {}", ERHI_LOG_PREFIX_UNKNOWN, pMessage) << std::endl;
 			} break;
 		}
 
