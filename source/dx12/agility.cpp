a7797acff3d702c6f0108e33ad5dcc9be0e010d9
lu-plus-plus <jllucs@zju.edu.cn>
2024-05-07 22:59:36 +0800

    [draft] import DX12 agility SDK

---
+++ source/dx12/agility.cpp
@@ -0,0 +1,4 @@
+#include "erhi/dx12/native.hpp"
+
+extern "C" { __declspec(dllexport) extern const UINT D3D12SDKVersion = 613; }
+extern "C" { __declspec(dllexport) extern const char * D3D12SDKPath = __FILE__ "\\..\\..\\packages\\D3D12Agility"; }
